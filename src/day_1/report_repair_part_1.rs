0a976d147d7c0ddfc0c9517171ce0b89264ed10b
mllanos <mario.llanos@appdirect.com>
2021-01-13 08:26:38 -0300

    Refactoring & adding tests for day 1.

--- src/day_1/report_repair_part_1.rs
+++ src/day_1/report_repair_part_1.rs
@@ -1,20 +1,34 @@
 use std::fs;
 
-pub fn run() {
-    let contents = fs::read_to_string("src/day_1/input.txt")
-        .expect("Something went wrong reading the file");
+#[test]
+fn validate() {
+    assert_eq!(algorithm("src/day_1/input_test.txt", false).0, 514579);
+}
+
+fn algorithm(file_location: &str, print_results: bool) -> (i32, usize) {
+    let contents = fs::read_to_string(file_location).unwrap();
     let values: Vec<&str> = contents.lines().collect();
     let mut count = 0;
+    let mut result = 0;
     for (i, x) in values.iter().enumerate() {
         let a: i32 = x.parse().unwrap();
         for y in values.clone().drain(i+1..values.len()) {
             count = count + 1;
             let b: i32 = y.parse().unwrap();
             if a + b == 2020 {
-                print!("{} + {} = 2020\n", a, b);
-                print!("{} * {} = {}\n", a, b, a * b);
+                result = a * b;
+                if print_results {
+                    println!("{} + {} = 2020", a, b);
+                    println!("{} * {} = {}", a, b, result);
+                }
             }
         }
     }
-    print!("Amount of iterations: {}\n", count);
+
+    (result, count)
+}
+
+pub fn run() {
+    let (result, iterations) = algorithm("src/day_1/input.txt", true);
+    println!("Result: {}. Amount of iterations: {}.", result, iterations);
 }
--- src/day_1/report_repair_part_2.rs
+++ src/day_1/report_repair_part_2.rs
@@ -1,10 +1,15 @@
 use std::fs;
 
-pub fn run() {
-    let contents = fs::read_to_string("src/day_1/input.txt")
-        .expect("Something went wrong reading the file");
+#[test]
+fn validate() {
+    assert_eq!(algorithm("src/day_1/input_test.txt", false).0, 241861950);
+}
+
+fn algorithm(file_location: &str, print_results: bool) -> (i32, usize) {
+    let contents = fs::read_to_string(file_location).unwrap();
     let values: Vec<&str> = contents.lines().collect();
     let mut count = 0;
+    let mut result = 0;
     for (i, x) in values.iter().enumerate() {
         let a: i32 = x.parse().unwrap();
         let mut count_inner = 0;
@@ -15,11 +20,20 @@ pub fn run() {
                 let c: i32 = z.parse().unwrap();
                 count = count + 1;
                 if a + b + c == 2020 {
-                    print!("{} + {} + {} = 2020\n", a, b, c);
-                    print!("{} * {} * {} = {}\n", a, b, c, a * b * c);
+                    result = a * b * c;
+                    if print_results {
+                        println!("{} + {} + {} = 2020", a, b, c);
+                        println!("{} * {} * {} = {}", a, b, c, result);
+                    }
                 }
             }
         }
     }
-    print!("Amount of iterations: {}\n", count);
+
+    (result, count)
+}
+
+pub fn run() {
+    let (result, iterations) = algorithm("src/day_1/input.txt", true);
+    println!("Result: {}. Amount of iterations: {}.", result, iterations);
 }
